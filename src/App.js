e22cca90b32fd9a87e7a5a70fee192cfa32f99bd
ipb21.t.rudzitis <ipb21.t.rudzitis@pjvt.local>
2023-10-31 10:43:01 +0200

    pabeigts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
 function App() {
   return (
     <div className="App">
-      <h1>Random teksts</h1>
+      <body>
+    <h1>English to Latvian Keywords</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>English Keyword</th>
+                <th>Latvian Equivalent(s)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>feature</td>
+                <td>Funkcionalitāte, Fīča</td>
+            </tr>
+            <tr>
+                <td>background</td>
+                <td>Konteksts, Situācija</td>
+            </tr>
+            <tr>
+                <td>scenario</td>
+                <td>Piemērs, Scenārijs</td>
+            </tr>
+            <tr>
+                <td>scenarioOutline</td>
+                <td>Scenārijs pēc parauga</td>
+            </tr>
+            <tr>
+                <td>examples</td>
+                <td>Piemēri, Paraugs</td>
+            </tr>
+            <tr>
+                <td>given</td>
+                <td>*</td>
+            </tr>
+            <tr>
+                <td>when</td>
+                <td>*</td>
+            </tr>
+            <tr>
+                <td>then</td>
+                <td>*</td>
+            </tr>
+            <tr>
+                <td>and</td>
+                <td>*</td>
+            </tr>
+            <tr>
+                <td>but</td>
+                <td>*</td>
+            </tr>
+            <tr>
+                <td>rule</td>
+                <td>Rule</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
     </div>
   );
 }
